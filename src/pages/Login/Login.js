7108cfa45d87b0653d95193ea408cd5087fb1356
arifibnenam <arifibnenam@gmail.com>
2022-05-28 22:18:47 +0600

    readme file added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -41,8 +41,7 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left w-full max-w-xl">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold lg:w-[400px]">Login now!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
